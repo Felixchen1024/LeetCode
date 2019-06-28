aa98498896178af6c1a50dba8cc3940ab1a4cb1f
Felixchen1024 <Felixchen1024@gmail.com>
2019-06-28 14:41:20 +0800

    13.roman-to-integer

---
+++ C++/13.roman-to-integer.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=13 lang=cpp
+ *
+ * [13] Roman to Integer
+ */
+class Solution {
+public:
+    int romanToInt(string s) {
+        if (s.empty()) 
+            return 0;
+        unordered_map<char, int> mp { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
+        int sum = mp[s.back()];
+        for (int i = s.size() - 1; i > 0; --i) {
+            sum += mp[s[i]] <= mp[s[i - 1]] ? mp[s[i-1]] : -mp[s[i-1]];
+        }
+        return sum;
+    }
+};
+
